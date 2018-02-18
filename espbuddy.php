e46e2bbcd3a9c9199b4fad5eed1e7b7d706b293f
soif <wxopwx@gmail.com>
2018-02-18 23:23:08 +0100

    Comments

--- espbuddy.php
+++ espbuddy.php
@@ -1,9 +1,19 @@
 #!/usr/bin/php
 <?php
 /*
-	--------------------------------
-	Copyright Francois Dechery 2018
-	--------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+EspBuddy - An Upload Toolbox for ESP8266 based devices
+--------------------------------------------------------------------------------------------------------------------------------------
+Copyright (C) 2018  by François Déchery - https://github.com/soif/
+
+EspBuddy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+EspBuddy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------------------------------------------
 */
 
 $config_file=dirname(__FILE__).'/config.php';
@@ -13,7 +23,8 @@ if(!file_exists($config_file)){
 }
 require($config_file);
 
-#############################################################
+
+// Main ###############################################################################################################################
 
 SwitchCommand($argv[1]);
 exit(0);

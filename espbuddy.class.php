e345c9730f8a288c02014b343060e19c9c532622
soif <wxopwx@gmail.com>
2018-02-19 14:13:09 +0100

    rename version property (to ease parsing)

--- espbuddy.class.php
+++ espbuddy.class.php
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License along with thi
 */
 class EspBuddy {
 
-	public $version				= '1.10';	// EspBuddy Version
+	public $class_version		= '1.10';	// EspBuddy Version
 	
 	private $cfg				= array();
 
